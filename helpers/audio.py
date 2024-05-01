7105ee47cf80044302a1e1ea5da3d5b14aa2be85
James Woglom <j@wogloms.net>
2024-05-01 00:52:53 -0400

    more robust switchaudio-osx support

--- helpers/audio.py
+++ helpers/audio.py
@@ -9,13 +9,13 @@ def mute(method=None, device=None):
     if method == 'applescript' or method is None:
         return run_applescript('set volume with output muted')
     elif method == 'switchaudio':
-        return run_switchaudio(device, ['-m', 'mute'])
+        return switchaudio_mute(device, 'mute')
 
 def unmute(method=None, device=None):
     if method == 'applescript' or method is None:
         return run_applescript('set volume without output muted')
     elif method == 'switchaudio':
-        return run_switchaudio(device, ['-m', 'unmute'])
+        return switchaudio_mute(device, 'unmute')
 
 def is_muted(method=None):
     out = run_applescript('get volume settings')
@@ -25,17 +25,27 @@ def is_muted(method=None):
         elif 'output muted:true' in out:
             return True
 
-
-def run_switchaudio(device, args):
-    # https://github.com/deweller/switchaudio-osx
-    params = []
+def switchaudio_mute(device, muting):
     if device:
-        params += ['-s', device]
-    else:
-        params += ['-c']
-    
-    params += args
+        current_device = run_switchaudio('-c')
+        if device.strip() != current_device.strip():
+            def mute_device(d):
+                run_switchaudio('-s', d)
+                return run_switchaudio('-m', muting)
+            if ',' in device:
+                ok = True
+                for d in device.split(','):
+                    ok = ok and mute_device(d)
+                run_switchaudio('-s', current_device)
+                return ok
+            else:
+                mute_device(device)
+                run_switchaudio('-s', current_device)
+                return ok
+    return run_switchaudio('-m', muting)
 
+# https://github.com/deweller/switchaudio-osx
+def run_switchaudio(*params):
     result = subprocess.run(['SwitchAudioSource', *params], capture_output=True, text=True)
     if result.returncode == 0 and result.stdout:
         return result.stdout.strip()
