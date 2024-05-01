cc508a5a7c3f80ebdf0264223c0eaa02d13d2c1d
James Woglom <j@wogloms.net>
2024-05-01 01:19:33 -0400

    still allow unknown mute state to mute or unmute

--- mlbtvmuter.py
+++ mlbtvmuter.py
@@ -39,7 +39,7 @@ unmuted_at = None
 
 def should_mute(args):
     global muted_at
-    if is_muted() == False:
+    if is_muted() in (False, None):
         logger.info('MUTING AUDIO')
         mute(args.audio_method, args.audio_device)
         muted_at = time.time()
@@ -48,7 +48,7 @@ def should_mute(args):
 
 def should_unmute(args):
     global unmuted_at
-    if is_muted() == True:
+    if is_muted() in (True, None):
         logger.info('UNMUTING AUDIO')
         unmute(args.audio_method, args.audio_device)
         unmuted_at = time.time()
