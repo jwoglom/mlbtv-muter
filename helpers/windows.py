92043a222ef3f2e5b1f20d13d5a9b213ad7943b7
James Woglom <j@wogloms.net>
2024-05-03 20:36:04 -0400

    fix nircmd

--- helpers/windows.py
+++ helpers/windows.py
@@ -11,7 +11,7 @@ def is_windows():
 def run_nircmd(*args):
     bin_folder = os.path.join(os.path.dirname(__file__), 'bin')
     logger.debug(f'calling nircmd with {bin_folder=}')
-    result = subprocess.run(['nircmd.exe', *args], capture_output=True, text=True, cwd=bin_folder)
+    result = subprocess.run([os.path.join(bin_folder, 'nircmd.exe'), *args], capture_output=True, text=True, cwd=bin_folder)
     if result.returncode == 0 and result.stdout:
         return result.stdout.strip()
     elif result.stderr:
